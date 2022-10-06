65514a9b631a62ca7eb8cc5bec88408673304444
Roma1125 <ramazonomonturdiyev17@gmail.com>
2022-10-06 09:33:28 +0500

    sasadasd

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,8 +1,6 @@
 #Create a variable called 'number' and assign it the two-digit number
 number=43
 #Find the reverse of the number and assign it to a variable called 'answer'.
-s=number%10
-d=number//10
-answer=str(s)+str(d)
+answer=str(number%10)+str(number//10)
 #Print the answer variable
 print(answer)
